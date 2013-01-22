b1f5879c796ceaaebd7119f69238273e68f8cbbd
subbarao <subbarao.kly@gmail.com>
2013-01-22 18:53:51 +0530

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterPostComments::Application.config.secret_token = '8cc5fcb3e66a967e44c60ca816350a90b694ef60cc97577e67af03a0ced55c9b6d46e93fa6eedbf4dc11454f5eb4082ba530446a2c6531bade52328eb7e1c037'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TwitterPostComments::Application.config.session_store :cookie_store, key: '_twitter_post_comments_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TwitterPostComments::Application.config.session_store :active_record_store
